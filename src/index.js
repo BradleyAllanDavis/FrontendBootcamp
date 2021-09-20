139ed1b5d042cf63b0ce06262cd2d180eba6b86b
Bradley Davis <Bradley_Davis@intuit.com>
2021-09-20 12:07:58 -0700

    Update Exercise 1 with better method

--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import { render } from "react-dom";
 import { HelloWorld } from './components/HelloWorld';
 import { CarTool } from './components/CarTool';
 
-render(<HelloWorld />, document.querySelector('#root'));
-render(<CarTool />, document.querySelector('#CarTool'));
-
-
-
-
-
+render(
+<>
+  <HelloWorld />
+  <CarTool />
+</>,
+document.querySelector('#root'));
 
 
