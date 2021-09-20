b7df4ee7d57b18b02419badafe82e9af1d77967b
Bradley Davis <Bradley_Davis@intuit.com>
2021-09-20 11:25:29 -0700

    Use jsx for function

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import { createElement } from "react";
 import { render } from "react-dom";
 
 const HelloWorld3 = () => {
-  return createElement('h1', null, 'Hello, World 3!');
+  // return createElement('h1', null, 'Hello, World 3!');
+  return <h1>Hello, World!</h1>;
 };
 
-render(createElement(HelloWorld3), document.querySelector('#root'));
+// render(createElement(HelloWorld3), document.querySelector('#root'));
+render(<HelloWorld3 />, document.querySelector('#root'));
 
 
 
