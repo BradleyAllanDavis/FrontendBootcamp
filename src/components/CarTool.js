dd5dec7a13e1842e4861bae07628da63c7118eb2
Bradley Davis <Bradley_Davis@intuit.com>
2021-09-20 16:48:42 -0700

    Exercise 5

--- src/components/CarTool.js
+++ src/components/CarTool.js
@@ -1,4 +1,23 @@
+import { useState } from 'react';
+
 export const CarTool = (props) => {
+
+  const [
+    carForm, // state data
+    setCarForm, // function to update the state data and re-render
+  ] = useState({
+    make: '', model: '', year: '', color: '', price: '',
+  });
+
+  const change = e => {
+    setCarForm({
+      ...carForm, // spread operator to copy the properties from the original color form to the new color form
+      [ e.target.name ]: e.target.value,
+      // hexcode: e.target.value
+    }); // new object literal, so we have a new object reference saying the state change
+  };
+
+  console.log(carForm);
 
   return (
     <>
@@ -27,6 +46,29 @@ export const CarTool = (props) => {
             </tr>)}
         </tbody>
       </table>
+      <form>
+        <label>
+          Car Make:
+          <input type="text" name="make" value={carForm.make} onChange={change} />
+        </label>
+        <label>
+          Car Model:
+          <input type="text" name="model" value={carForm.model} onChange={change} />
+        </label>
+        <label>
+          Car Year:
+          <input type="text" name="year" value={carForm.year} onChange={change} />
+        </label>
+        <label>
+          Car Color:
+          <input type="text" name="color" value={carForm.color} onChange={change} />
+        </label>
+        <label>
+          Car Price:
+          <input type="text" name="price" value={carForm.price} onChange={change} />
+        </label>
+        <button type="button">Add Car</button>
+      </form>
     </>
   );
 };
