b9ddfac4c8fde5611d4b32280666565b07468aa2
Bradley Davis <Bradley_Davis@intuit.com>
2021-09-20 14:42:36 -0700

    Use dynamic list

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import { render } from "react-dom";
 
-import { HelloWorld } from './components/HelloWorld';
 import { CarTool } from './components/CarTool';
 import { ColorTool } from "./components/ColorTool";
 
 render(
 <>
   <CarTool />
+  <ColorTool />
 </>,
 document.querySelector('#root'));
 
