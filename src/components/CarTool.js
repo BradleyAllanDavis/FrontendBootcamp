275f3739119c1ea91839e42fb9df829b32cd047c
Bradley Davis <Bradley_Davis@intuit.com>
2021-09-20 15:13:59 -0700

    Exercise 3

--- src/components/CarTool.js
+++ src/components/CarTool.js
@@ -1,4 +1,10 @@
 export const CarTool = () => {
+
+  const cars = [
+    { id: 1, make: 'Tesla', model: 'Model 3', year: 2019, color: 'Silver', price: 60000 },
+    { id: 2, make: 'Tesla', model: 'Model Y', year: 2021, color: 'Blue', price: 65000 },
+  ];
+
   return (
     <>
       <header>
@@ -16,22 +22,14 @@ export const CarTool = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Tesla</td>
-            <td>Model 3</td>
-            <td>2019</td>
-            <td>Silver</td>
-            <td>$55,000</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Tesla</td>
-            <td>Model Y</td>
-            <td>2021</td>
-            <td>Blue</td>
-            <td>$59,000</td>
-          </tr>
+          {cars.map(car => <tr key={car.id}>
+            <td>{car.id}</td>
+            <td>{car.make}</td>
+            <td>{car.model}</td>
+            <td>{car.year}</td>
+            <td>{car.color}</td>
+            <td>{car.price}</td>
+            </tr>)}
         </tbody>
       </table>
     </>
