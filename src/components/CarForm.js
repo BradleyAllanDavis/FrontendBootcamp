b81c961a25bdcd1ad4ed5d94529a721d20da88f1
Bradley Davis <Bradley_Davis@intuit.com>
2021-09-21 14:56:37 -0700

    Exercise 8

---
+++ src/components/CarForm.js
@@ -0,0 +1,61 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+
+export const CarForm = ({ buttonText, onSubmitCar }) => {
+
+  const [
+    carForm, // state data
+    setCarForm, // function to update the state data and re-render
+  ] = useState({
+    make: '',
+    model: '', 
+    year: 1900, 
+    color: '', 
+    price: 0,
+  });
+
+  const change = e => {
+    setCarForm({
+      ...carForm,
+      [ e.target.name ]: e.target.value,
+    });
+  };
+
+  const submitCar = () => {
+    onSubmitCar({ ...carForm });
+    setCarForm({
+      make: '',
+      model: '', 
+      year: 1900, 
+      color: '', 
+      price: 0,
+    });
+  };
+
+  return (
+    <form>
+      <label>
+        Car Make:
+        <input type="text" name="make" value={carForm.make} onChange={change} />
+      </label>
+      <label>
+        Car Model:
+        <input type="text" name="model" value={carForm.model} onChange={change} />
+      </label>
+      <label>
+        Car Year:
+        <input type="text" name="year" value={carForm.year} onChange={change} />
+      </label>
+      <label>
+        Car Color:
+        <input type="text" name="color" value={carForm.color} onChange={change} />
+      </label>
+      <label>
+        Car Price:
+        <input type="text" name="price" value={carForm.price} onChange={change} />
+      </label>
+      <button type="button" onClick={submitCar}>{buttonText}</button>
+    </form>
+  );
+
+};
--- src/components/CarTool.js
+++ src/components/CarTool.js
@@ -1,71 +1,27 @@
 import { useState } from 'react';
 import { ToolHeader } from './ToolHeader';
 import { CarTable } from './CarTable';
+import { CarForm } from './CarForm';
 
 export const CarTool = (props) => {
 
   const [ cars, setCars ] = useState([ ...props.cars]);
   
-  const [
-    carForm, // state data
-    setCarForm, // function to update the state data and re-render
-  ] = useState({
-    make: '',
-    model: '', 
-    year: 1900, 
-    color: '', 
-    price: 0,
-  });
-
-  const change = e => {
-    setCarForm({
-      ...carForm,
-      [ e.target.name ]: e.target.value,
-    });
-  };
-
-  const addCar = () => {
+  const addCar = (newCar) => {
     setCars([
       ...cars,
-      { ...carForm, id: Math.max(...cars.map(c => c.id), 0) + 1 },
+      {
+        ...newCar,
+        id: Math.max(...cars.map(c => c.id), 0) + 1
+      },
     ]);
-
-    setCarForm({
-      make: '',
-      model: '',
-      year: 1900,
-      color: '',
-      price: 0,
-    });
   }
 
   return (
     <>
       <ToolHeader header='CarTool'/>
       <CarTable cars={cars}/>
-      <form>
-        <label>
-          Car Make:
-          <input type="text" name="make" value={carForm.make} onChange={change} />
-        </label>
-        <label>
-          Car Model:
-          <input type="text" name="model" value={carForm.model} onChange={change} />
-        </label>
-        <label>
-          Car Year:
-          <input type="text" name="year" value={carForm.year} onChange={change} />
-        </label>
-        <label>
-          Car Color:
-          <input type="text" name="color" value={carForm.color} onChange={change} />
-        </label>
-        <label>
-          Car Price:
-          <input type="text" name="price" value={carForm.price} onChange={change} />
-        </label>
-        <button type="button" onClick={addCar}>Add Car</button>
-      </form>
+      <CarForm buttonText='Add Car' onSubmitCar={addCar}/>
     </>
   );
 };
