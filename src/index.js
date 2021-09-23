67f2e27e7db98525f2478db26f10456e037edf70
Bradley Davis <Bradley_Davis@intuit.com>
2021-09-23 15:02:06 -0700

    Add css file

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const colorList = [
 ]; // array literal
 
 const carList = [
-  { id: 1, make: 'Tesla', model: '3', year: 2019, color: 'Silver', price: 60000 },
+  { id: 1, make: 'Tesla', model: '3', year: 2019, color: 'Silver', price: 47000 },
   { id: 2, make: 'Ford', model: 'T', year: 1910, color: 'Blue', price: 10000 },
   { id: 3, make: 'Tesla', model: 'Y', year: 2021, color: 'Black', price: 65000 },
   { id: 4, make: 'GM', model: 'Sierra', year: 2005, color: 'White', price: 34000 },
